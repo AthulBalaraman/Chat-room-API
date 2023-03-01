1bd15f487e909d8bc486528e2f5d3bd56a58d2f3
AthulBalaraman <athul2522001@gmail.com>
2023-03-01 11:57:50 +0530

    added authentication

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express()
+const errorHandlers = require('./handlers/errorHandler')
+const db = require('./config/db')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const MessageModel = require("./models/Message")
+const ChatroomModel = require("./models/Chatroom")
+const cookieParser = require('cookie-parser')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+// Database connection
+db(()=>{
+  try {
+      console.log("DataBase Successfully Connected");        
+  } catch (error) {
+      console.log("Database Not Connected : ", error);        
+  }
+});
+
+
+//Bring in the routes
+app.use("/user",require('./routes/user'))
+
+
+// Setup Error handlers
+app.use(errorHandlers.notFound)
+app.use(errorHandlers.mongoseErrors)
+if(process.env.ENV === "DEVELOPMENT")
+{
+  app.use(errorHandlers.developmentErrors)
+}else{
+  app.use(errorHandlers.productionErrors)
+}
+
+
+
+
+app.listen(5000,()=>{
+  console.log("Server running at port 5000")
+})
+
+
---
+++ controllers/userController.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const UserModel = require("../models/User");
+const sha256 = require("js-sha256");
+const {createTokens,validateToken} = require('../handlers/JWT')
+
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const emailRegex = /@gmail.com|@yahoo.com|@hotmail.com|@live.com/;
+  if (!emailRegex.test(email)) throw "Email is not supported from your domain";
+  if (password.length < 6) {
+    throw "Password should atleast be 6 characters long";
+  }
+
+  const userExists = await UserModel.findOne({
+    email:email
+  });
+  if(userExists) throw "User with the same email exists."
+
+  const user = new UserModel({
+    name,
+    email,
+    password: sha256(password + process.env.SALT),
+  });
+  await user.save();
+
+  res.json({
+    user: name,
+    message: "User registered successfully",
+  });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({
+    email,
+    password: sha256(password + process.env.SALT),
+  });
+  if(!user) throw "Email and Password did not match."
+
+  const accessToken = createTokens(user)
+
+  res.cookie("access-token", accessToken,{
+    maxAge:60*60*24*30*1000,
+    httpOnly:true
+  })
+
+  res.json({
+    message:"User logged in successfully!",
+    token
+  })
+};
